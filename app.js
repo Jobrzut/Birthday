aa77f087af837097064fae5283fc96771dc16da7
Julek <90630830+Jobrzut@users.noreply.github.com>
2022-08-20 23:03:29 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+
+function niezgoda() {
+    document.getElementById('ybt').innerHTML
+    = 'That is a pitty, comeback here someday!';
+    
+    document.getElementById('hey').style.display = 'none';
+    document.getElementById('yes').style.display = 'none';
+    document.getElementById('no').style.display = 'none';
+    document.getElementById('sad').style.display = 'block';
+}
+
+function zgoda() {
+    document.getElementById('ybt').innerHTML
+    = 'I have something for u, open it!';
+    
+    document.getElementById('hey').style.display = 'none';
+    document.getElementById('yes').style.display = 'none';
+    document.getElementById('no').style.display = 'none';
+    document.getElementById('gift').style.display = 'block';
+}
+
+function opened() {
+    document.getElementById('ybt').innerHTML
+    = 'Happy Birthday!!!';
+    document.getElementById('hey').style.display = 'none';
+    document.getElementById('yes').style.display = 'none';
+    document.getElementById('no').style.display = 'none';
+    document.getElementById('gift').style.display = 'none';
+    document.getElementById('party').style.display = 'block';
+}
+
